316bcc7c7bfb6a984b6bd7686507107c695d7a50
Anmolbaral <58747993+Anmolbaral@users.noreply.github.com>
2025-09-13 02:15:26 -0500

    Updated file cachng system

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 from modules.rag import RAGPipeline
 from modules.loader import load_pdf
+from modules.hashing import get_file_hash
 
 import os, json
 
 allFiles = os.listdir("data")
 allTexts = []
-fileMetadata = []
-for file in allFiles:
-	fileName = "data/" + file
-	docs = load_pdf(fileName)
-	fileMetadata.append({
-		"fileName": fileName,
-		"fileSize": os.path.getsize(fileName),
-		"fileModifiedTime": os.path.getmtime(fileName),
-		"fileContent": docs
-	})
-	allTexts.extend(docs)
-	with open("cache/file_metadata.json", "w") as f:
-		json.dump(fileMetadata, f, indent=4)
+fileMetadata = {}
 
-rag = RAGPipeline(allTexts)
+# Check if the file metadata is valid
+# No need to make a new embedding cache if the file metadata is valid
+def cache_is_valid():
+	try:
+		with open("cache/file_metadata.json", "r") as f:
+			cacheMetadata = json.load(f)
+		
+		for file in allFiles:
+			filePath = f"data/{file}"
+
+			if file not in cacheMetadata:
+				return False
+
+			if get_file_hash(filePath) != cacheMetadata[file]["fileHash"]:
+				return False
+		return True
+	except Exception as e:
+		print(f"Error checking cache: {e}")
+		return False
 
 while True:
 	query = input("Ask a question: ")
+	
+	#if file metadata is invalid, we make a new embedding cache
+	if cache_is_valid():
+		print("Using cached embeddings")
+		rag = RAGPipeline.from_cache()
+	else:
+		for file in allFiles:
+			fileLoc = "data/" + file
+			docs = load_pdf(fileLoc)
+			fileMetadata[file] = ({
+				"fileSize": os.path.getsize(fileLoc),
+				"fileModifiedTime": os.path.getmtime(fileLoc),
+				"fileHash": get_file_hash(fileLoc),
+			})
+			allTexts.extend(docs)
+			with open("cache/file_metadata.json", "w") as f:
+				json.dump(fileMetadata, f, indent=4)
+		print("Using new embeddings")
+		rag = RAGPipeline(allTexts)
 	answer = rag.ask(query)
 	print("Answer: ", answer)
--- modules/rag.py
+++ modules/rag.py
@@ -1,15 +1,34 @@
 from modules.embeddings import get_embedding, get_embeddings
 from modules.vectordb import VectorDB
-import openai, os
+import openai, os, json
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class RAGPipeline:
 	def __init__(self, chunks):
 		self.db = VectorDB(dim=1536)
-		texts = [chunk["text"] for chunk in chunks]
-		embeddings = get_embeddings(texts)
+		self.chunks = chunks
+		self.chunkCount = len(chunks)
+		self.texts = [chunk["text"] for chunk in chunks]
+		self.embeddings = get_embeddings(self.texts)
+
 		for i, t in enumerate(chunks):
-			self.db.add(embeddings[i], t["text"], t["metadata"])
+			self.db.add(self.embeddings[i], t["text"], t["metadata"])
+		self.add_to_cache()
+	
+
+	@classmethod
+	def from_cache(cls, cacheFile = "cache/embeddings.json"):
+		with open(cacheFile, "r") as f:
+			metadata = json.load(f)
+
+		obj = cls.__new__(cls)
+		obj.db = VectorDB(dim=1536)
+		obj.texts = metadata["texts"]
+		obj.embeddings = metadata["embeddings"]
+		for i, t in enumerate(obj.texts):
+			obj.db.add(obj.embeddings[i], t, {"chunkId":i})
+		return obj
+	
 	
 	def ask(self, query):
 		queryEmb = get_embedding(query)
@@ -24,3 +43,14 @@ class RAGPipeline:
 			messages = [{"role": "user", "content": prompt}]
 		)
 		return response.choices[0].message.content
+	
+	def add_to_cache(self):
+		metadata = {
+			"chunkCount": self.chunkCount,
+			"texts": self.texts,
+			"embeddings": self.embeddings
+		}
+		with open("cache/embeddings.json", "w") as f:
+			json.dump(metadata, f, indent=4)
+			
+
